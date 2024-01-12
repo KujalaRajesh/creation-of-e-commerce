33f41d49093577133054528d5ded99fcf131a425
TEST <TEST@-PC>
2024-01-12 11:58:18 +0530

    singup

---
+++ src/main/java/com/aventstack/extentreports/ExtentReports.java
@@ -0,0 +1,17 @@
+package com.aventstack.extentreports;
+
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReports {
+
+	public void attachReporter(ExtentSparkReporter spark) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
